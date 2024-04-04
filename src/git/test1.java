6bdb2be6e0fa8856b51971a2dcc914205cb8971d
Navaneeth krishnan S <navaneethkrishnans2000@gmail.com>
2024-04-04 21:49:21 +0530

    first commit

---
+++ src/git/test1.java
@@ -0,0 +1,15 @@
+package git;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		boolean hunger = true;
+		
+		if(hunger == true)
+			System.out.println("Eat Something..");
+		else
+			System.out.println("Do Work..");
+	}
+
+}
